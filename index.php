caf443f8fa68cf2e10de4dd41a2349b82744c92a
Thomas Hale <tomhale247@gmail.com>
2025-04-12 17:49:02 -0500

    showing off git

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quote System CSCI 467</title>
+    <title>Quote System CSCI 467 test</title>
     <link rel="stylesheet" href="/assets/css/styles.css">
 </head>
 <body>
