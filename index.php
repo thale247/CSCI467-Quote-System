888ab14e308c905f8a4b329b9e3333b362c6183b
Eduardo Acosta <laloacodelreal@gmail.com>
2025-04-17 19:15:42 -0500

    Test removed

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quote System CSCI 467 test</title>
+    <title>Quote System CSCI 467</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
